26a37a9aa03941d39412042df19cde4c972cb0f1
Gustavo <111918084+gust8vo@users.noreply.github.com>
2022-09-12 16:48:09 -0300

    Banco de Dados - Estoque

---
+++ Spectrum/src/controller/jdbc_DB.java
@@ -0,0 +1,20 @@
+
+package controller;
+
+public class jdbc_DB {
+    
+    // Estoque
+    
+    public static int pao = 1500; // Gramas
+    public static int pao_vegano = 300; // Gramas
+    public static int bacon = 180; // Gramas
+    public static int tomate = 1635; // Gramas (109g) /5 - 22g
+    public static int cebola = 1680; // Gramas (112g) /5 - 22g
+    public static int alface = 3750; // Gramas (250g) /5 - 16g
+    public static int cola = 11250; // Mililitro
+    public static int suco = 7500; // Mililitro
+    public static int batata_frita = 3750; // Gramas
+    public static int nuggets = 3750; // Gramas
+
+    
+}
